7af1e3b51705e050d4bc94c114230b73e7d6a058
Phillip Maier <pmaier983@gmail.com>
2019-08-27 09:58:26 -0400

    Edit Format, Basic Back end working
    user's can now add notes for me through the back-end

---
+++ backend/models/user.model.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      minlength: 3
+    },
+    email: {
+      type: String,
+      required: true
+    },
+    note: {
+      type: String,
+      required: true
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
