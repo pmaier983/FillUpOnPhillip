06f037a0abb84ab57d13b135519536895d738e45
Phillip Maier <pmaier983@gmail.com>
2019-08-27 09:58:26 -0400

    Edit Format, Basic Back end working
    user's can now add notes for me through the back-end

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-// const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 require("dotenv").config();
 
@@ -10,19 +10,16 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// const uri = process.env.ATLAS_URI;
-// mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
-// );
-// const connection = mongoose.connection;
-// connection.once('open', () => {
-//   console.log("MongoDB database connection established successfully");
-// })
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
 
-// const exercisesRouter = require('./routes/exercises');
-// const usersRouter = require('./routes/users');
+const usersRouter = require("./routes/users");
 
-// app.use('/exercises', exercisesRouter);
-// app.use('/users', usersRouter);
+app.use("/users", usersRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
