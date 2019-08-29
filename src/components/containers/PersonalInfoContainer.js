6793ff03cc41f3cc45a4c6991beaf5fde0a5407a
Phillip Maier <pmaier983@gmail.com>
2019-08-29 09:55:00 -0400

    Format Personal Page
    Also changed Profile Picture

--- src/components/containers/PersonalInfoContainer.js
+++ src/components/containers/PersonalInfoContainer.js
@@ -1,8 +1,41 @@
 import React from "react"
 import styled from "styled-components"
 
+import { colors } from "../utils/theme"
+
+const PictureOfSelf = require("../../static/PictureOfSelf.png")
+
+const InfoContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`
+
+const TextDescriptionContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-self: center;
+  width: 100%;
+  margin: 0 15px;
+`
+
+const ProfilePicture = styled.img`
+  width: 50%;
+  border-radius: 50%;
+  border: 15px dashed ${colors.lightJellyBeanRed};
+`
+
 const PersonalInfoContainer = () => {
-  return <div>Page About Myself</div>
+  return (
+    <InfoContainer>
+      <TextDescriptionContainer>
+        <div>Phillip ED Maier</div>
+        <div>Software Engineer</div>
+        <div>Whats up</div>
+      </TextDescriptionContainer>
+      <ProfilePicture src={PictureOfSelf} />
+    </InfoContainer>
+  )
 }
 
 export default PersonalInfoContainer
