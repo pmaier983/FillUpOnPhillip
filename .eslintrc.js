8b748178aae11b265e5f5ad5683b77f0bde2f77b
Phillip Maier <pmaier983@gmail.com>
2020-03-03 21:13:23 -0500

    Add Forgotten Config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,12 @@
-const OFF = 0, WARN = 1, ERROR = 2
+const OFF = 0,
+  WARN = 1,
+  ERROR = 2
 
 module.exports = {
   env: {
     browser: true,
     es6: true,
-    jest: true,
+    jest: true
   },
   extends: ["airbnb"],
   globals: {
@@ -20,8 +22,38 @@ module.exports = {
   },
   plugins: ["react"],
   parser: "babel-eslint",
+  "settings": {
+    "import/resolver": {
+      "node": {
+        "extensions": [
+          ".js",
+          ".jsx",
+          ".ts",
+          ".tsx"
+        ]
+      }
+    }
+  },
   rules: {
     "react/jsx-props-no-spreading": OFF,
     "react/require-default-props": OFF,
-  },
+    "react/prop-types": OFF,
+    semi: ["error", "never"],
+    "space-infix-ops": OFF,
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        "js": "never",
+        "jsx": "never",
+        "ts": "never",
+        "tsx": "never"
+      }
+    ],
+    "react/jsx-filename-extension": [
+      1,
+      { extensions: [".jsx", ".tsx"] }
+    ]
+  }
 }
+
