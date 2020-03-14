4f0caa8b905ba70b8dcd9e3b24f90c9ad45addfc
Phillip Maier <pmaier983@gmail.com>
2020-03-14 15:17:43 -0400

    Format Index, Update Layouts

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import AppWrapper from './AppWrapper';
-import App from './App';
+import AppWrapper from './AppWrapper'
+import App from './App'
 
 ReactDOM.render(
   <AppWrapper>
     <App />
   </AppWrapper>,
   document.getElementById('root'),
-);
+)
--- src/layouts.ts
+++ src/layouts.ts
@@ -1,3 +1,5 @@
+import { IBreakpointLayouts } from './hooks/useGridLayouts.d'
+
 export const columns = {
   lg: 2, md: 2, sm: 2, xs: 2, xxs: 2,
 }
@@ -6,7 +8,7 @@ export const breakpoints = {
   lg: 1200, md: 1024, sm: 768, xs: 568, xxs: 320,
 }
 
-export const layouts = {
+export const initialGridLayouts: IBreakpointLayouts = {
   lg: [{
     x: 0,
     y: 0,
