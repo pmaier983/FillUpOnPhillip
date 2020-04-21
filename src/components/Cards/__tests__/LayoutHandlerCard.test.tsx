d9c518efb9bb69eac14957bd9a4c4a7de4831df7
Phillip Maier <pmaier983@gmail.com>
2020-04-21 18:53:26 -0400

    Import extend-expect on every file

--- src/components/Cards/__tests__/LayoutHandlerCard.test.tsx
+++ src/components/Cards/__tests__/LayoutHandlerCard.test.tsx
@@ -3,7 +3,6 @@ import {
   render,
   fireEvent,
 } from '@testing-library/react'
-import '@testing-library/jest-dom/extend-expect'
 
 import { ThemesProvider } from '../../../contexts'
 import ThemeWrapper from '../../../ThemeWrapper'
