32def38d0f931b0744560ec22e21e336e12bd253
Phillip Maier <pmaier983@gmail.com>
2020-04-24 17:37:11 -0400

    Remove useTimeBoolean Hook

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -1,5 +1,4 @@
 export { default as useBoolean } from './useBoolean'
-export { default as useTimedBoolean } from './useTimedBoolean'
 export { default as useQuery } from './useQuery'
 export { default as useLocalStorage } from './useLocalStorage'
 export { default as useGridLayouts } from './useGridLayouts'
