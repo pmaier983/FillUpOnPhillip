bbb11bf27991bfd983d89d26d3137074289ca735
Phillip Maier <pmaier983@gmail.com>
2020-04-26 11:49:07 -0400

    Update TestDraggability and add support and plugin

--- cypress/integration/TestDraggability.spec.js
+++ cypress/integration/TestDraggability.spec.js
@@ -2,7 +2,7 @@ context('Test Draggability', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000/')
   })
-  it('Click is draggable', () => {
+  it('Can Drag when draggable', () => {
     cy.get('[data-testid="PersonalCard"]').then((PersonalCard) => PersonalCard[0].getBoundingClientRect()).then((personalCardBounds) => {
       cy.get('[data-testid="PersonalCard"]').within(() => {
         cy.get('[data-testid="card-handle"]').trigger('mousedown', { which: 1 })
@@ -19,5 +19,33 @@ context('Test Draggability', () => {
         })
       })
     })
+  })
+  it('Cannot Drag when Not-draggable', () => {
+    cy.get('[data-testid="toggle-draggability"]').click()
+      .then(() => {
+        cy.get('[data-testid="PersonalCard"]')
+          .then((PersonalCard) => PersonalCard[0].getBoundingClientRect())
+          .then((personalCardBounds) => {
+            cy.get('[data-testid="PersonalCard"]').within(() => {
+              cy.get('[data-testid="card-handle"]').trigger('mousedown', { which: 1 })
+            })
+              .then(() => {
+                cy.get('[data-testid="LayoutHandlerCard"]')
+                  .trigger('mousemove')
+                  .trigger('mouseup', { force: true })
+              })
+              .then(() => {
+                cy.get('[data-testid="PersonalCard"]').then((PersonalCard) => PersonalCard[0].getBoundingClientRect())
+                  .then((newPersonalCardBounds) => {
+                    expect(newPersonalCardBounds.x).to.equal(personalCardBounds.x)
+                    // TODO: why does this go to 0 at one point
+                    // I think it has something to do with how react-grid-layout handles dragging
+                    // expect(newPersonalCardBounds.y).to.equal(personalCardBounds.y)
+                    expect(newPersonalCardBounds.width).to.equal(personalCardBounds.width)
+                    expect(newPersonalCardBounds.height).to.equal(personalCardBounds.height)
+                  })
+              })
+          })
+      })
   })
 })
