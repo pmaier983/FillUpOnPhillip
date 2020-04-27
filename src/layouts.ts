a9b9af8facca811ecd93475843f91e825accf029
Phillip Maier <pmaier983@gmail.com>
2020-04-27 09:15:09 -0400

    Update Layouts

--- src/layouts.ts
+++ src/layouts.ts
@@ -1,7 +1,7 @@
 import { IBreakpointLayouts } from './hooks/useGridLayouts.d'
 
 export const columns = {
-  lg: 4, md: 4, sm: 3, xs: 1, xxs: 1,
+  lg: 4, md: 4, sm: 3, xs: 2, xxs: 1,
 }
 
 export const breakpoints = {
@@ -11,70 +11,68 @@ export const breakpoints = {
 // TODO make an array for the i
 export const initialGridLayouts: IBreakpointLayouts = {
   lg: [{
-    w: 1, h: 15, x: 0, y: 0, i: 'PersonalCard', moved: false, static: false,
+    w: 1, h: 14, x: 0, y: 0, i: 'PersonalCard', static: false,
   }, {
-    w: 2, h: 9, x: 0, y: 15, i: 'RepositoriesCard', moved: false, static: false,
+    w: 2, h: 8, x: 2, y: 0, i: 'RepositoriesCard', static: false,
   }, {
-    w: 1, h: 5, x: 1, y: 10, i: 'ContactCard', moved: false, static: false,
+    w: 1, h: 5, x: 1, y: 0, i: 'ContactCard', static: false,
   }, {
-    w: 1, h: 6, x: 1, y: 4, i: 'WhatMattersCard', moved: false, static: false,
+    w: 2, h: 8, x: 0, y: 14, i: 'WhatMattersCard', static: false,
   }, {
-    w: 1, h: 4, x: 1, y: 0, i: 'LayoutHandlerCard', moved: false, static: false,
-  },
-  {
-    w: 1, h: 4, x: 1, y: 0, i: 'TechUsedCard', moved: false, static: false,
-  },
-  ],
+    w: 1, h: 9, x: 1, y: 5, i: 'LayoutHandlerCard', static: false,
+  }, {
+    w: 2, h: 14, x: 2, y: 8, i: 'TechUsedCard', static: false,
+  }],
   md: [{
-    w: 1, h: 11, x: 0, y: 0, i: 'PersonalCard', moved: false, static: false,
+    w: 1, h: 13, x: 0, y: 0, i: 'PersonalCard', static: false,
   }, {
-    w: 2, h: 10, x: 2, y: 5, i: 'RepositoriesCard', moved: false, static: false,
+    w: 2, h: 9, x: 2, y: 13, i: 'RepositoriesCard', static: false,
   }, {
-    w: 2, h: 4, x: 0, y: 11, i: 'ContactCard', moved: false, static: false,
+    w: 1, h: 3, x: 1, y: 0, i: 'ContactCard', static: false,
   }, {
-    w: 1, h: 11, x: 1, y: 0, i: 'WhatMattersCard', moved: false, static: false,
+    w: 2, h: 9, x: 0, y: 13, i: 'WhatMattersCard', static: false,
   }, {
-    w: 2, h: 5, x: 2, y: 0, i: 'LayoutHandlerCard', moved: false, static: false,
+    w: 1, h: 10, x: 1, y: 3, i: 'LayoutHandlerCard', static: false,
   }, {
-    w: 1, h: 4, x: 1, y: 0, i: 'TechUsedCard', moved: false, static: false,
+    w: 2, h: 13, x: 2, y: 0, i: 'TechUsedCard', static: false,
   }],
   sm: [{
-    w: 1, h: 11, x: 0, y: 0, i: 'PersonalCard', moved: false, static: false,
+    w: 1, h: 11, x: 0, y: 0, i: 'PersonalCard', static: false,
   }, {
-    w: 2, h: 10, x: 0, y: 11, i: 'RepositoriesCard', moved: false, static: false,
+    w: 1, h: 8, x: 1, y: 0, i: 'RepositoriesCard', static: false,
   }, {
-    w: 1, h: 3, x: 2, y: 4, i: 'ContactCard', moved: false, static: false,
+    w: 1, h: 3, x: 0, y: 11, i: 'ContactCard', static: false,
   }, {
-    w: 1, h: 7, x: 1, y: 4, i: 'WhatMattersCard', moved: false, static: false,
+    w: 1, h: 8, x: 0, y: 14, i: 'WhatMattersCard', static: false,
   }, {
-    w: 2, h: 4, x: 1, y: 0, i: 'LayoutHandlerCard', moved: false, static: false,
+    w: 1, h: 8, x: 2, y: 0, i: 'LayoutHandlerCard', static: false,
   }, {
-    w: 1, h: 4, x: 1, y: 0, i: 'TechUsedCard', moved: false, static: false,
+    w: 2, h: 14, x: 1, y: 8, i: 'TechUsedCard', static: false,
   }],
   xs: [{
-    x: 0, y: 0, w: 1, h: 11, i: 'PersonalCard',
+    w: 1, h: 10, x: 0, y: 0, i: 'PersonalCard', static: false,
   }, {
-    x: 0, y: 2, w: 2, h: 9, i: 'RepositoriesCard',
+    w: 1, h: 5, x: 0, y: 17, i: 'RepositoriesCard', static: false,
   }, {
-    x: 1, y: 1, w: 1, h: 5, i: 'ContactCard',
+    w: 1, h: 3, x: 1, y: 19, i: 'ContactCard', static: false,
   }, {
-    x: 1, y: 0, w: 1, h: 6, i: 'WhatMattersCard',
+    w: 1, h: 8, x: 1, y: 0, i: 'WhatMattersCard', static: false,
   }, {
-    x: 1, y: 0, w: 1, h: 6, i: 'LayoutHandlerCard',
+    w: 1, h: 7, x: 0, y: 10, i: 'LayoutHandlerCard', static: false,
   }, {
-    w: 1, h: 4, x: 1, y: 0, i: 'TechUsedCard', moved: false, static: false,
+    w: 1, h: 11, x: 1, y: 8, i: 'TechUsedCard', static: false,
   }],
   xxs: [{
-    w: 1, h: 11, x: 0, y: 0, i: 'PersonalCard', moved: false, static: false,
+    w: 1, h: 11, x: 0, y: 0, i: 'PersonalCard', static: false,
   }, {
-    w: 2, h: 9, x: 0, y: 17, i: 'RepositoriesCard', moved: false, static: false,
+    w: 1, h: 8, x: 0, y: 38, i: 'RepositoriesCard', static: false,
   }, {
-    w: 1, h: 5, x: 1, y: 12, i: 'ContactCard', moved: false, static: false,
+    w: 1, h: 3, x: 0, y: 17, i: 'ContactCard', static: false,
   }, {
-    w: 1, h: 6, x: 1, y: 0, i: 'WhatMattersCard', moved: false, static: false,
+    w: 1, h: 6, x: 0, y: 11, i: 'WhatMattersCard', static: false,
   }, {
-    w: 1, h: 6, x: 1, y: 6, i: 'LayoutHandlerCard', moved: false, static: false,
+    w: 1, h: 5, x: 0, y: 20, i: 'LayoutHandlerCard', static: false,
   }, {
-    w: 1, h: 4, x: 1, y: 0, i: 'TechUsedCard', moved: false, static: false,
+    w: 1, h: 13, x: 0, y: 25, i: 'TechUsedCard', static: false,
   }],
 }
