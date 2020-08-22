b92d9675f43977f45fc68787005ad3dd737b6cc4
Phillip Maier <pmaier983@gmail.com>
2020-08-22 12:44:16 -0400

    Fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,60 +1,62 @@
-const OFF = 0,
-  WARN = 1,
-  ERROR = 2
+const OFF = 0
+
+/* eslint-disable */
+const WARN = 1
+const ERROR = 2
+/* eslint-enable */
 
 module.exports = {
   env: {
     browser: true,
     es6: true,
-    jest: true
+    jest: true,
   },
-  extends: ["airbnb", "plugin:cypress/recommended"],
+  extends: ['airbnb', 'plugin:cypress/recommended'],
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly"
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 2018,
-    sourceType: "module"
+    sourceType: 'module',
   },
-  plugins: ["react"],
-  parser: "babel-eslint",
-  "settings": {
-    "import/resolver": {
-      "node": {
-        "extensions": [
-          ".js",
-          ".jsx",
-          ".ts",
-          ".tsx"
-        ]
-      }
-    }
+  plugins: ['react'],
+  parser: 'babel-eslint',
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: [
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+        ],
+      },
+    },
   },
   rules: {
-    "react/jsx-props-no-spreading": OFF,
-    "react/require-default-props": OFF,
-    "react/prop-types": OFF,
+    'react/jsx-props-no-spreading': OFF,
+    'react/require-default-props': OFF,
+    'react/prop-types': OFF,
     'import/prefer-default-export': OFF,
-    semi: ["error", "never"],
-    "space-infix-ops": OFF,
-    "import/extensions": [
-      "error",
-      "ignorePackages",
+    semi: ['error', 'never'],
+    'space-infix-ops': OFF,
+    'import/extensions': [
+      'error',
+      'ignorePackages',
       {
-        "js": "never",
-        "jsx": "never",
-        "ts": "never",
-        "tsx": "never"
-      }
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
     ],
-    "react/jsx-filename-extension": [
+    'react/jsx-filename-extension': [
       1,
-      { extensions: [".jsx", ".tsx"] }
-    ]
-  }
+      { extensions: ['.jsx', '.tsx'] },
+    ],
+  },
 }
-
